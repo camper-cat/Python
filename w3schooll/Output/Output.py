7f509e820aac65ab083b6e8b3bea011e759390c1
Кіт Турист <travel.cat90@gmail.com>
2025-11-04 01:01:33 +0200

    Add: Folder Output with Output.py

---
+++ w3schooll/Output/Output.py
@@ -0,0 +1,65 @@
+# Output
+    # Print Without a New Line with end=" "
+# print("Hello World!", end=" ")
+# print("I will print on the same line.")
+
+    # Print Numbers
+# print(3)
+# print(123)
+    #Abd functions
+# print(1 + 3)
+# print(5 * 6)
+
+    #Mix Text and Numbers
+# print("I'm", 27, "years old" )
+
+# Output End
+
+
+# Start of GPT Exercises
+    #Start of Ex 1
+        # 🧩 Завдання 1. Просте виведення тексту
+        #
+        # Мета: навчитися використовувати функцію print() для виведення тексту.
+        #
+        # Умова:
+        # Напиши програму, яка виведе на екран три рядки:
+        # I am learning Python
+        # Python is fun!
+        # Let's code!
+# print("I am learning Python")
+# print("Python is fun!")
+# print("Let's code!")
+    # End of Ex 1
+
+    #Start of Ex 2
+    # Завдання 2. Виведення кількох значень
+    #
+    # Мета: навчитися виводити кілька значень одним викликом print().
+    #
+    # Умова:
+    # Створи дві змінні:
+    #
+    # name = "Alex"
+    # age = 25
+    #
+    #
+    # Виведи на екран речення:
+    #
+    # My name is Alex and I am 25 years old.
+# name = "Alex"
+# age = 27
+# print("My name is", name, "and I am", age, "years old")
+    #End of Ex 2
+
+    # Start of Ex 3
+        # 🧩 Завдання 3. Кінець рядка і роздільник
+        #
+        # Мета: зрозуміти параметри end і sep у print().
+        #
+        # Умова:
+        # Виведи числа від 1 до 3 в один рядок через дефіс:
+# print(1, 2, 3, sep="-")
+# print(1, end="-")
+# print(2, end="-")
+# print(3)
