c4afb46c7f75f2db47027f2602a4c2b9ab911d28
Кіт Турист <travel.cat90@gmail.com>
2025-11-28 19:11:49 +0200

    Add: Folder - Beginers; Add: ex_1.py

---
+++ Pynative/Beginners/ex_1.py
@@ -0,0 +1,10 @@
+# Start of Exercise 1
+
+number1 = int(input("Enter number 1: "))
+number2 = int(input("Enter number 2: "))
+product = number1 * number2
+#якщо тип = int, то
+if(product <= 1000):
+    print(product)
+else:
+    print(number1 + number2)
