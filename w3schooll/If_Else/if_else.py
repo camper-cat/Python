2d8222496f986222c59282d412f0e596bb310be7
Кіт Турист <travel.cat90@gmail.com>
2025-11-29 13:19:38 +0200

    Add: if_else.py

---
+++ w3schooll/If_Else/if_else.py
@@ -0,0 +1,38 @@
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+# 1st u need to use what (have to do), if...
+#                        (Print())
+
+
+#
+
+a = 10
+b = 20
+bigger = a if a > b else b
+print("Bigger is", bigger)
+
+#
+
+username = ""
+display_name = username if username else "Guest"
+print("Welcome,", display_name)
+
+#
+
+
+#
+
+age = 25
+is_student = False
+has_discount_code = True
+
+if (age < 18 or age > 65) and not is_student or has_discount_code:
+  print("Discount applies!")
+
+#
+
+
+#
+
